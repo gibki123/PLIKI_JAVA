d6faf816fbfc5d0fe6fc27a8fbdf64093678e184
gibki123 <maciej.krol16@interia.pl>
2018-04-15 11:20:11 +0200

    uzupelnienie

--- Pierwszy_projekt/src/Main.java
+++ Pierwszy_projekt/src/Main.java
@@ -1,7 +1,34 @@
 public class Main {
     public static void main(String[] args)
     {
-        public static void 
+        /*
+        Student maciek = new Student();
+        maciek.id = 1;
+        maciek.wiek = 19;
+        maciek.imie = "Maciek";
+
+        Student tomek = new Student();
+        tomek.id = 2;
+        tomek.wiek = 21;
+        tomek.imie = "Tomek";
+
+        System.out.println(tomek.imie + " is " + tomek.wiek + " years old");
+        System.out.println(maciek.imie + " is " + maciek.wiek + " years old");
+        */
+        Student maciek = new Student();
+        maciek.setId(1);
+        maciek.setWiek(19);
+        maciek.setImie("Maciek");
+
+        Student tomek = new Student();
+        tomek.setId(2);
+        tomek.setWiek(21);
+        tomek.setImie("Tomek");
+
+        System.out.println(tomek.getImie() + " is " + tomek.getWiek() + " years old");
+        System.out.println(maciek.getImie() + " is " + maciek.getWiek() + " years old");
+
+
     }
 
 
